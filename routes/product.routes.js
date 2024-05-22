3e4ab2c39f2e4e222550c850f3bba8d0f2ce5086
David Ivanoski <davidivanoski1@live.com>
2024-05-22 10:36:49 +0200

    product.routes code refractor with async/await functions

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,79 +5,109 @@ const mongoose = require("mongoose");
 const Product = require("../models/Product.model");
 
 // POST - Create new product
-router.post("/products", (req, res, next) => {
-  const { title, description, price } = req.body;
-  Product.create({ title, description, price })
-    .then((response) => res.json(response))
-    .catch((err) => {
-      console.log("error creating product", err);
-      res.status(500).json({ message: "Error while creating the product" });
-    });
+router.post("/products", async (req, res, next) => {
+  try {
+    const { title, description, price } = req.body;
+
+    if (!title || !description || !price) {
+      return res
+        .status(400)
+        .json({ message: "Title, description and price are required" });
+    }
+    const newProduct = await Product.create({ title, description, price });
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.error("error creating product", error);
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res
+        .status(400)
+        .json({ message: "Invalid data from product creation", error });
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 // GET - All products
-router.get("/products", (req, res, next) => {
-  Product.find()
-    .then((allProducts) => res.json(allProducts))
-    .catch((err) => {
-      console.log("error creating product", err);
-      res.status(500).json({ message: "Error while getting the products" });
-    });
+router.get("/products", async (req, res, next) => {
+  try {
+    const allProdcuts = await Product.find();
+    res.json(allProdcuts);
+  } catch (error) {
+    console.error("Error getting products", error);
+    res.status(500).json({ message: "Error while getting the products" });
+  }
 });
 
 // GET - One product
-router.get("/products/:productId", (req, res, next) => {
-  const { productId } = req.params;
+router.get("/products/:productId", async (req, res, next) => {
+  try {
+    const { productId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(productId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ message: "Specified id is not valid" });
+    }
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error retriving product:", error);
+    res.status(500).json({ message: "Error while retriving the product" });
   }
-
-  Product.findById(productId)
-    .then((product) => res.status(200).json(product))
-    .catch((err) => {
-      console.log("Error while retrieving the product", err);
-      res.status(500).json({ message: "Error while retrieving the product" });
-    });
 });
 
 // PUT - Update a specific product by ID
-router.put("/products/:productId", (req, res, next) => {
-  const { productId } = req.params;
+router.put("/products/:productId", async (req, res, next) => {
+  try {
+    const { productId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(productId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ message: "Specified id is not valid" });
+    }
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .json({ message: "Product not found or could not be updated" });
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ message: "Error while updating the product" });
   }
-
-  Product.findByIdAndUpdate(productId, req.body, { new: true })
-    .then((updatedProduct) => res.json(updatedProduct))
-    .catch((err) => {
-      console.log("Error while updating the product", err);
-      res.status(500).json({ message: "Error while updating the product" });
-    });
 });
 
 // DELETE - Delete one product
-router.delete("/products/:productId", (req, res, next) => {
-  const { productId } = req.params;
+router.delete("/products/:productId", async (req, res, next) => {
+  try {
+    const { productId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(productId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ message: "Specified ID is not valid" });
+    }
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res
+        .status(404)
+        .json({ message: "Product not found or could not be deleted" });
+    }
+    res
+      .status(200)
+      .json({ message: `Product with ID ${productId} has been removed` });
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ message: "Error while deleting the product" });
   }
-
-  Product.findByIdAndDelete(productId)
-    .then(() =>
-      res.json({
-        message: `Product with ${productId} is removed successfully.`,
-      })
-    )
-    .catch((err) => {
-      console.log("Error while deleting the product", err);
-      res.status(500).json({ message: "Error while deleting the product" });
-    });
 });
 
 module.exports = router;
